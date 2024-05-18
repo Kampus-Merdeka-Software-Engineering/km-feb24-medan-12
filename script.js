d01c0f5e3dfb8ab8033e6f5d8647e6e93bf3c32f
Khaerina009 <khaerinafadillah@gmail.com>
2024-05-18 21:00:25 +0700

    melengkapi code sales composition

--- script.js
+++ script.js
@@ -1,203 +1,206 @@
 //Total Revenue By Tax Class
-const chart4 = document.getElementById('mychart_4')
+const chart4 = document.getElementById("mychart_4");
 
 //TOP 10 TOTAL REVENUE AND TRANSACTION BY NEIGHBORHOOD CHART
-const chart6 = document.getElementById('mychart_6');
+const chart6 = document.getElementById("mychart_6");
 
-fetch('File Json/Total_revenue_transaction_neighborhood.json')
-.then(function(response) {
-
-    if(response.ok == true){
-        return response.json();
+fetch("File Json/Total_revenue_transaction_neighborhood.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
     }
-})
-.then(function(data){
-  console.log(data);
-  var arrTotalSales = [];
-  var arrTotalTransaction= [];
-  var arrNeighborhood = [];
-  data.forEach(element => {
-    arrTotalSales.push(element.TOTAL_SALES);
-    arrTotalTransaction.push(element.TOTAL_TRANSACTIONS);
-    arrNeighborhood.push(element.NEIGHBORHOOD);
+  })
+  .then(function (data) {
+    console.log(data);
+    var arrTotalSales = [];
+    var arrTotalTransaction = [];
+    var arrNeighborhood = [];
+    data.forEach((element) => {
+      arrTotalSales.push(element.TOTAL_SALES);
+      arrTotalTransaction.push(element.TOTAL_TRANSACTIONS);
+      arrNeighborhood.push(element.NEIGHBORHOOD);
+    });
+    console.log(arrTotalSales);
+    console.log(arrTotalTransaction);
+    var objChart = {
+      total_sales: arrTotalSales,
+      total_transaction: arrTotalTransaction,
+      neighborhood: arrNeighborhood,
+    };
+    console.log(objChart);
+    createChart(objChart, "bar");
   });
-  console.log(arrTotalSales);
-  console.log(arrTotalTransaction);
-  var objChart = {
-    total_sales : arrTotalSales,
-    total_transaction : arrTotalTransaction,
-    neighborhood : arrNeighborhood
-  };
-  console.log(objChart);
-  createChart(objChart, 'bar');
-})
 
-fetch('File Json/Total_Revenue_by_Tax_Class.json')
-.then(function(response) {
-
-    if(response.ok == true){
-        return response.json();
+fetch("File Json/Total_Revenue_by_Tax_Class.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
     }
-})
-.then(function(data){
+  })
+  .then(function (data) {
     console.log(data);
     var arrTotalRevenue = [];
     var arrTaxClass = [];
-    data.forEach(element => {
+    data.forEach((element) => {
       arrTotalRevenue.push(element.TOTAL_REVENUE);
       arrTaxClass.push(element.TAX_CLASS);
     });
     console.log(arrTaxClass);
     console.log(arrTotalRevenue);
     var objChart = {
-      tax_class : arrTaxClass,
-      total_revenue : arrTotalRevenue,
+      tax_class: arrTaxClass,
+      total_revenue: arrTotalRevenue,
     };
     console.log(objChart);
-    createChart2(objChart, 'bar');
-})
+    createChart2(objChart, "bar");
+  });
 
-function createChart(arrPassed, type){
+function createChart(arrPassed, type) {
   new Chart(chart6, {
     type: type,
     data: {
       labels: arrPassed.neighborhood,
-      datasets: [{
-        label: 'Total Sales',
-        data: arrPassed.total_sales,
-        borderWidth: 1,
-        yAxisID: "sales",
-      },
-      {
-        label: 'Transactions',
-        data: arrPassed.total_transaction,
-        borderWidth: 1,
-        yAxisID: "transaction"
-      }
-    ]
+      datasets: [
+        {
+          label: "Total Sales",
+          data: arrPassed.total_sales,
+          borderWidth: 1,
+          yAxisID: "sales",
+        },
+        {
+          label: "Transactions",
+          data: arrPassed.total_transaction,
+          borderWidth: 1,
+          yAxisID: "transaction",
+        },
+      ],
     },
     options: {
       plugins: {
         title: {
           display: true,
-          text: 'TOP 10 TOTAL REVENUE & TRANSACTION BY NEIGHBORHOOD'
-        }
+          text: "TOP 10 TOTAL REVENUE & TRANSACTION BY NEIGHBORHOOD",
+        },
       },
       layout: {
-        padding: {
-        }
+        padding: {},
       },
       scales: {
         y: {
           beginAtZero: true,
-          display: false
+          display: false,
         },
-        'sales':{
-          type: 'linear',
-          position: 'left',
+        sales: {
+          type: "linear",
+          position: "left",
           min: 0,
           max: 400000000,
           title: {
             display: true,
-            text: 'Total Sales'
-          }
+            text: "Total Sales",
+          },
         },
-        'transaction'  : {
-          type: 'linear',
-          position: 'right',
-          min:  0,
+        transaction: {
+          type: "linear",
+          position: "right",
+          min: 0,
           max: 1000,
           display: true,
           title: {
             display: true,
-            text: 'Total Transactions'
-          }
-        }
-      }
-    }
+            text: "Total Transactions",
+          },
+        },
+      },
+    },
   });
 }
 
-function createChart2(arrPassed2, type){
+function createChart2(arrPassed2, type) {
   new Chart(chart4, {
     type: type,
     data: {
       labels: arrPassed2.tax_class,
-      datasets: [{
-        label: 'Total Revenue',
-        data: arrPassed2.total_revenue,
-        borderWidth: 1,
-      },
-    ]
+      datasets: [
+        {
+          label: "Total Revenue",
+          data: arrPassed2.total_revenue,
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       plugins: {
         title: {
-          display : true,
-          text : 'TOTAL REVENUE BY TAX CLASS'
-        }
+          display: true,
+          text: "TOTAL REVENUE BY TAX CLASS",
+        },
       },
       scales: {
         y: {
-          beginAtZero: true
+          beginAtZero: true,
         },
-      }
-    }
+      },
+    },
   });
 }
 
 // Quarterly Sales Revenue and Transactions Chart
 function createQuarterlySalesChart() {
-  fetch('File Json/Quarterly_Sales_revenue.json')
-    .then(function(response) {
+  fetch("File Json/Quarterly_Sales_revenue.json")
+    .then(function (response) {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       var arrRevenue = [];
       var arrQuarters = [];
 
-      data.forEach(element => {
+      data.forEach((element) => {
         arrRevenue.push(element.TOTAL_REVENUE);
-        arrQuarters.push(`Q${element.EXTRACTED_QUARTER} ${element.EXTRACTED_YEAR}`);
+        arrQuarters.push(
+          `Q${element.EXTRACTED_QUARTER} ${element.EXTRACTED_YEAR}`
+        );
       });
 
       var objChart = {
         total_revenue: arrRevenue,
-        quarters: arrQuarters
+        quarters: arrQuarters,
       };
 
-      createQuarterlyChart(objChart, 'line'); // Tipe chart sekarang adalah 'line'
+      createQuarterlyChart(objChart, "line"); // Tipe chart sekarang adalah 'line'
     })
-    .catch(function(error) {
-      console.error('Error during fetch operation:', error);
+    .catch(function (error) {
+      console.error("Error during fetch operation:", error);
     });
 }
 
 function createQuarterlyChart(arrPassed, type) {
-  const ctx = document.getElementById('mychart_1');
+  const ctx = document.getElementById("mychart_1");
   new Chart(ctx, {
     type: type, // Sekarang adalah grafik garis
     data: {
       labels: arrPassed.quarters,
-      datasets: [{
-        label: 'Total Revenue',
-        data: arrPassed.total_revenue,
-        borderColor: 'rgb(0, 0, 255)',
-        backgroundColor: 'rgba(0, 0, 255, 0.1)',
-        borderWidth: 1,
-        fill: false, // Pastikan area di bawah garis tidak diisi
-        tension: 0.4 // Menambahkan kelengkungan pada garis
-      }]
+      datasets: [
+        {
+          label: "Total Revenue",
+          data: arrPassed.total_revenue,
+          borderColor: "rgb(0, 0, 255)",
+          backgroundColor: "rgba(0, 0, 255, 0.1)",
+          borderWidth: 1,
+          fill: false, // Pastikan area di bawah garis tidak diisi
+          tension: 0.4, // Menambahkan kelengkungan pada garis
+        },
+      ],
     },
     options: {
       plugins: {
         title: {
           display: true,
-          text: 'Quarterly Sales Revenue'
-        }
+          text: "Quarterly Sales Revenue",
+        },
       },
       scales: {
         y: {
@@ -205,12 +208,12 @@ function createQuarterlyChart(arrPassed, type) {
           max: 1400000000, // Atur nilai maksimal untuk pendapatan
           title: {
             display: true,
-            text: 'Total Revenue (in USD)'
-          }
-        }
+            text: "Total Revenue (in USD)",
+          },
+        },
       },
-      responsive: true
-    }
+      responsive: true,
+    },
   });
 }
 
@@ -218,134 +221,131 @@ function createQuarterlyChart(arrPassed, type) {
 createQuarterlySalesChart();
 
 //Total Revenue By Building Category
-const chart5 = document.getElementById('mychart_5')
+const chart5 = document.getElementById("mychart_5");
 
-fetch('File Json/Total_Revenue_building_Category.json')
-.then(function(response) {
-    
-    if(response.ok == true) {
-        return response.json();
+fetch("File Json/Total_Revenue_building_Category.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
     }
-})
-.then(function(data) {
-  console.log(data);
-  var arrTotalRevenue = [];
-  var arrBuildingClassCategory = [];
-  var arrRevenuePercentage = [];
-  data.forEach(element => {
+  })
+  .then(function (data) {
+    console.log(data);
+    var arrTotalRevenue = [];
+    var arrBuildingClassCategory = [];
+    var arrRevenuePercentage = [];
+    data.forEach((element) => {
       arrTotalRevenue.push(element.Total_Revenue);
       arrBuildingClassCategory.push(element.BUILDING_CLASS_CATEGORY);
-      arrRevenuePercentage.push(element.Revenue_Percentage);       
+      arrRevenuePercentage.push(element.Revenue_Percentage);
+    });
+    console.log(arrBuildingClassCategory);
+    console.log(arrTotalRevenue);
+    console.log(arrRevenuePercentage);
+    var objChart = {
+      building_class_category: arrBuildingClassCategory,
+      total_revenue: arrTotalRevenue,
+      revenue_percentage: arrRevenuePercentage,
+    };
+    console.log(objChart);
+    createChart5(objChart, "bar");
   });
-  console.log(arrBuildingClassCategory);
-  console.log(arrTotalRevenue);
-  console.log(arrRevenuePercentage);
-  var objChart = {
-      building_class_category : arrBuildingClassCategory,
-      total_revenue : arrTotalRevenue,
-      revenue_percentage : arrRevenuePercentage
-  };
-  console.log(objChart);
-  createChart5(objChart, 'bar');
-})
-
 
 function createChart5(arrPassed5, type) {
   new Chart(chart5, {
-    type: 'bar', 
+    type: "bar",
     data: {
       labels: arrPassed5.building_class_category,
-      datasets: [{
-        label: 'Total Revenue',
-        data: arrPassed5.total_revenue,
-        borderColor: 'rgb(0, 0, 255)',
-        backgroundColor: 'rgba(0, 0, 255, 0.1)',
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: "Total Revenue",
+          data: arrPassed5.total_revenue,
+          borderColor: "rgb(0, 0, 255)",
+          backgroundColor: "rgba(0, 0, 255, 0.1)",
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
-      indexAxis: 'y',
+      indexAxis: "y",
       scales: {
-          x: {
-              type: 'logarithmic',
-          },
+        x: {
+          type: "logarithmic",
+        },
       },
       plugins: {
         title: {
           display: true,
-          text: 'Total Revenue By Building Category'
-        }
+          text: "Total Revenue By Building Category",
+        },
       },
-    }
-  })
+    },
+  });
 }
 
 //MONTHLY AVERAGE REVENUE
-const chart2 = document.getElementById('mychart_2');
+const chart2 = document.getElementById("mychart_2");
 
-fetch('File Json/Monthly_Average_Revenue.json')
-.then(function(response) {
-
-    if(response.ok == true){
-        return response.json();
+fetch("File Json/Monthly_Average_Revenue.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
     }
-})
-.then(function(data){
+  })
+  .then(function (data) {
     console.log(data);
     var arrYearMonth = [];
     var arrAverageSalePrice = [];
-    data.forEach(element => {
+    data.forEach((element) => {
       arrYearMonth.push(element.YEAR_MONTH);
       arrAverageSalePrice.push(element.AVERAGE_SALE_PRICE);
     });
     console.log(arrYearMonth);
     console.log(arrAverageSalePrice);
     var objChart = {
-      YEAR_MONTH : arrYearMonth,
-      AVERAGE_SALE_PRICE : arrAverageSalePrice,
-
+      YEAR_MONTH: arrYearMonth,
+      AVERAGE_SALE_PRICE: arrAverageSalePrice,
     };
     console.log(objChart);
-    createChart7(objChart, 'line');
-})
+    createChart7(objChart, "line");
+  });
 
-function createChart7(arrLine3, type){
-
-    new Chart(chart2, {
-        type: type,
-        data: {
-          labels: arrLine3.YEAR_MONTH,
-          datasets: [{
-            label: 'Monthly_Average_Revenue',
-            data: arrLine3.AVERAGE_SALE_PRICE,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-          }]
+function createChart7(arrLine3, type) {
+  new Chart(chart2, {
+    type: type,
+    data: {
+      labels: arrLine3.YEAR_MONTH,
+      datasets: [
+        {
+          label: "Monthly_Average_Revenue",
+          data: arrLine3.AVERAGE_SALE_PRICE,
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
         },
-        options: {
-          plugins: {
-            title: {
-              display: true,
-              text: 'MONTHLY AVERAGE REVENUE'
-            }
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "MONTHLY AVERAGE REVENUE",
+        },
+      },
+      scales: {
+        y: {
+          display: true,
+          ticks: {
+            beginAtZero: true,
+            max: 800000,
+            min: 500000,
           },
-          scales: {
-            y: {
-            display: true,
-            ticks: {
-                beginAtZero: true,
-                max: 800000,
-                min: 500000
-            }
-             
-              }
-            }
-          }
-        }
-      )
-    }
-    
-// SALES COMPOSITION 
+        },
+      },
+    },
+  });
+}
+
+// SALES COMPOSITION
 const ctx = document.getElementById("mychart_3");
 
 fetch("File Json/Sales_Composition_building_classification (1).json")
@@ -371,3 +371,37 @@ fetch("File Json/Sales_Composition_building_classification (1).json")
     console.log(objChart);
     createChart3(objChart, "pie");
   });
+
+function createChart3(arrPassed, type) {
+  new Chart(ctx, {
+    type: type,
+    data: {
+      labels: arrPassed.building_class,
+      datasets: [
+        {
+          label: "Total Revenue",
+          data: arrPassed.total_revenue,
+          backgroundColor: [
+            "rgb(285, 109, 132)",
+            "rgb(89, 89, 119)",
+            "rgb(169, 76, 119)",
+            "rgb(17, 78, 117)",
+            "rgb(241, 186, 124)",
+          ],
+          hoverOffset: 5,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: "SALES COMPOSITION BUILDING CLASSIFICATION",
+        },
+        legend: {
+          position: "right",
+        },
+      },
+    },
+  });
+}
